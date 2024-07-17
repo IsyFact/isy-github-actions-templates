17951744031604e71c135dab875a78c9342a7b19
Marius Arlt <marius.arlt@msg.group>
2024-07-17 12:22:22 +0200

    chore: IFS-3767: remade white list

--- ort-config/evaluator.rules.kts
+++ ort-config/evaluator.rules.kts
@@ -1,13 +1,22 @@
 val allowedLicenses = licenseClassifications.licensesByCategory["allow"].orEmpty()
 
-rule("LicenseWhitelist") {
-    licenseFindings.forEach { finding ->
-        val license = finding.license
-        if (license !in allowedLicenses) {
-            issue(
-                    severity = Severity.ERROR,
-                    message = "License $license is not in the allowed list."
-            )
-        }
+fun RuleSet.wrongLicenseInLicenseFileRule() = projectSourceRule("WRONG_LICENSE_IN_LICENSE_FILE_RULE") {
+    require {
+        +projectSourceHasFile("LICENSE")
+    }
+
+    val detectedRootLicenses = projectSourceGetDetectedLicensesByFilePath("LICENSE").values.flatten().toSet()
+    val wrongLicenses = detectedRootLicenses - allowedLicenses
+
+    if (wrongLicenses.isNotEmpty()) {
+        error(
+                message = "The file 'LICENSE' contains the following disallowed licenses ${wrongLicenses.joinToString()}.",
+                howToFix = "Please use only the following allowed licenses: ${allowedLicenses.joinToString()}."
+        )
+    } else if (detectedRootLicenses.isEmpty()) {
+        error(
+                message = "The file 'LICENSE' does not contain any license which is not allowed.",
+                howToFix = "Please use one of the following allowed licenses: ${allowedLicenses.joinToString()}."
+        )
     }
 }
